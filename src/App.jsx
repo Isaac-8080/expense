7ef596671679e6d9a222edfe6552a601d6f4ab86
Isaac Opoku <isaacmanuopoku@gmail.com>
2024-11-29 02:48:29 +0000

    updated made

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
-import { useState } from 'react'
 import CardsContainer from './components/CardsContainer'
-import ExpenseItem from './components/ExpenseItem'
 import NewExpense from './components/NewExpense'
-import Form from './Form'
 import './index.css'
-import UserList from './UserList'
 
 function App() {
-
-  // const [collectFormData, setcollectFormData] = useState([])
-
-  // const handleFormData = (formData) => {
-  //   setcollectFormData([...collectFormData, formData])
-  // }  
-
 
   return (
 
     <>
-    
-      {/* <div className='h-screen flex items-cente gap-10 justify-center pt-20'>
-        <Form formData={handleFormData} />
-        <UserList list={collectFormData} />
-      </div> */}
 
       <CardsContainer>
         <NewExpense />
